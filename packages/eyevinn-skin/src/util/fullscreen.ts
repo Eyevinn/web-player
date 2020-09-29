a301f786402637a100b2d6b7a5d2bae27d3e567a
Benjamin Wallberg <me@bwallberg.com>
2020-09-29 21:20:36 +0200

    fix: fullscreen isFullscreen() not working

--- packages/eyevinn-skin/src/util/fullscreen.ts
+++ packages/eyevinn-skin/src/util/fullscreen.ts
@@ -1,16 +1,17 @@
-document.fullscreenElement;
-
 export function isFullscreen() {
-	return (
-		(document.fullscreenElement ||
-			(document as any).webkitFullscreenElement) !== null
+	return !!(
+		document.fullscreenElement || (document as any).webkitFullscreenElement
 	);
 }
 
 export function requestFullscreen(element: HTMLElement) {
-	(element.requestFullscreen || (element as any).webkitRequestFullscreen)?.call(element);
+	(element.requestFullscreen || (element as any).webkitRequestFullscreen)?.call(
+		element
+	);
 }
 
 export function exitFullscreen() {
-	(document.exitFullscreen || (document as any).webkitExitFullscreen)?.call(document);
+	(document.exitFullscreen || (document as any).webkitExitFullscreen)?.call(
+		document
+	);
 }
