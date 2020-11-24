9e131a4d52cb1d52e455f0c7cba40642196d8707
Benjamin Wallberg <me@bwallberg.com>
2020-11-24 15:50:27 +0100

    fix: formatting

--- packages/airplay/src/index.ts
+++ packages/airplay/src/index.ts
@@ -2,8 +2,8 @@ import EventEmitter from './util/EventEmitter';
 
 export enum AirPlayEvent {
 	AVAILABILITY_CHANGED = 'airplay:availability_changed',
-	TARGET_CHANGED = 'airplay:target_changed'
-} 
+	TARGET_CHANGED = 'airplay:target_changed',
+}
 
 export class AirPlay extends EventEmitter {
 	private video: HTMLVideoElement;
@@ -14,24 +14,35 @@ export class AirPlay extends EventEmitter {
 		if (!(window as any).WebKitPlaybackTargetAvailabilityEvent) {
 			this.emit(AirPlayEvent.AVAILABILITY_CHANGED, { available: false });
 		} else {
-			video.addEventListener('webkitplaybacktargetavailabilitychanged', this.onAvailabilityChanged = this.onAvailabilityChanged.bind(this));
-			video.addEventListener('webkitcurrentplaybacktargetiswirelesschanged', this.onTargetChanged = this.onTargetChanged.bind(this));
+			video.addEventListener(
+				'webkitplaybacktargetavailabilitychanged',
+				(this.onAvailabilityChanged = this.onAvailabilityChanged.bind(this))
+			);
+			video.addEventListener(
+				'webkitcurrentplaybacktargetiswirelesschanged',
+				(this.onTargetChanged = this.onTargetChanged.bind(this))
+			);
 		}
 	}
 
 	onAvailabilityChanged({ availability }: any) {
-			this.emit(AirPlayEvent.AVAILABILITY_CHANGED, { available: availability === 'available' });
+		this.emit(AirPlayEvent.AVAILABILITY_CHANGED, {
+			available: availability === 'available',
+		});
 	}
 
 	onTargetChanged(event: any) {
-		this.emit(AirPlayEvent.TARGET_CHANGED, { wireless: true })
+		this.emit(AirPlayEvent.TARGET_CHANGED, { wireless: true });
 	}
 
 	toggleAirPlay() {
-		(this.video as any).webkitShowPlaybackTargetPicker();	
+		(this.video as any).webkitShowPlaybackTargetPicker();
 	}
 
 	destroy() {
-		this.video.removeEventListener('webkitplaybacktargetavailabilitychanged', this.onAvailabilityChanged);
+		this.video.removeEventListener(
+			'webkitplaybacktargetavailabilitychanged',
+			this.onAvailabilityChanged
+		);
 	}
 }
