af33dff9e4289a154555fec104c32681de47aa55
SebastianLjungman <sebastian.ljungman94@gmail.com>
2022-01-24 13:09:29 +0100

    Added starttime attribute used in connectedCallback Changed muted and autoplay from bools to only attributes, and attributeChangedCallback to reflect this

--- packages/web-player-component/src/index.js
+++ packages/web-player-component/src/index.js
@@ -6,7 +6,7 @@ import { renderEyevinnSkin } from '@eyevinn/web-player-eyevinn-skin';
 
 export default class PlayerComponent extends HTMLElement {
   static get observedAttributes() {
-    return ['source', 'muted', 'autoplay'];
+    return ['source', 'starttime', 'muted', 'autoplay'];
   };
   constructor() {
     //Call constructor of HTMLElement
@@ -29,33 +29,39 @@ export default class PlayerComponent extends HTMLElement {
       castAppId: {}
     });
   }
-  
+
   //Handles changed attributes of player-component. Changes video attributes directly (not through player functions).
   attributeChangedCallback(name) {
-    console.log(name);
+    console.log("attributeChangedCallback: " + name);
     if (name === 'source') {
-      this.player.load(this.getAttribute('source'));
+      if (this.hasAttribute('source')) {
+        this.player.load(this.getAttribute('source'));
+      }
+      else {
+        console.log("Please provide a valid source!");
+      }
     }
     if (name === 'muted') {
-      if (this.getAttribute('muted') === 'true') {
+      if (this.hasAttribute("muted")) {
         this.video.muted = true;
       }
-      else if (this.getAttribute('muted') === 'false') {
+      else {
         this.video.muted = false;
       }
     }
     if (name === 'autoplay') {
-      if (this.getAttribute('autoplay') === 'true') {
-        this.video.setAttributeNode(document.createAttribute('autoplay'));
+      if (this.hasAttribute('autoplay')) {
+        this.video.setAttribute('autoplay', 'autoplay');
       }
-      else if (this.getAttribute('autoplay') === 'false') {
+      else {
         this.video.removeAttribute('autoplay');
       }
     }
   }
-
+  //Only applies starttime on page load
   connectedCallback() {
-    console.log("Connected!");
+    this.video.currentTime = this.getAttribute('starttime');
+    console.log("connectedCallback");
   }
 
   disconnectedCallback() {
