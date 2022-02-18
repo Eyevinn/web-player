a61095a0d48e6359451ee59fc533c56f61ddfd62
birme <jonas.birme@eyevinn.se>
2022-02-18 17:27:45 +0100

    chore: change sample streams to VODs that are more stable

--- packages/demo/src/index.js
+++ packages/demo/src/index.js
@@ -132,7 +132,7 @@ async function main() {
   }
   hlsButton.onclick = async () => {
     manifestInput.value =
-      'https://d2fz24s2fts31b.cloudfront.net/out/v1/6484d7c664924b77893f9b4f63080e5d/manifest.m3u8';
+      'https://f53accc45b7aded64ed8085068f31881.egress.mediapackage-vod.eu-north-1.amazonaws.com/out/v1/1c63bf88e2664639a6c293b4d055e6bb/ade303f83e8444d69b7658f988abb054/2a647c0cf9b7409598770b9f11799178/manifest.m3u8';
     load();
     resetEmbed();
     if (isClipboardAvailable()) {
@@ -141,7 +141,7 @@ async function main() {
   };
   dashButton.onclick = async () => {
     manifestInput.value =
-      'https://d2fz24s2fts31b.cloudfront.net/out/v1/3b6879c0836346c2a44c9b4b33520f4e/manifest.mpd';
+      'https://f53accc45b7aded64ed8085068f31881.egress.mediapackage-vod.eu-north-1.amazonaws.com/out/v1/1c63bf88e2664639a6c293b4d055e6bb/64651f16da554640930b7ce2cd9f758b/66d211307b7d43d3bd515a3bfb654e1c/manifest.mpd';
     load();
     resetEmbed();
     if (isClipboardAvailable()) {
