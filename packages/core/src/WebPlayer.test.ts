63ef7838f629fd96690894f026f0b9cff07b97af
Jonas Birmé <jonas.birme@eyevinn.se>
2022-03-07 17:12:37 +0100

    chore: a foundation for unit tests (#61)
    * chore: a foundation for unit tests
    
    * chore: run all the packages tests
    
    * chore: added HlsJsTech decision test
    
    * chore: run tests on push
    
    * chore: need to install dev dependencies on test
    
    * chore: need to add jest as direct dependency
    
    * chore: test on lts
    
    * chore: should match jest version with types
    
    * chore: remove mock reset that is not in use
    
    * chore: test content type decisioning

---
+++ packages/core/src/WebPlayer.test.ts
@@ -0,0 +1,57 @@
+import * as contentType from "./util/contentType";
+import * as browser from "./util/browser";
+import BaseTech from "./tech/BaseTech";
+jest.mock("./tech/BaseTech");
+import HlsJsTech from "./tech/HlsJsTech";
+jest.mock("./tech/HlsJsTech");
+
+import WebPlayer from "./WebPlayer";
+import { ManifestType } from "./util/constants";
+
+describe("WebPlayer core", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("chooses BaseTech if HLS and Safari", async () => {
+    jest.spyOn(contentType, "getManifestType").mockImplementation(async () => ManifestType.HLS);
+    jest.spyOn(contentType, "canPlayManifestType").mockImplementation(() => true);
+    jest.spyOn(browser, "isSafari").mockImplementation(() => true );
+    jest.spyOn(BaseTech.prototype, "load").mockImplementation(async () => { });
+
+    const videoElement = window.document.createElement("video");
+    const player = new WebPlayer({ video: videoElement });
+    await player.load("mock-stream");
+    expect(BaseTech).toHaveBeenCalledTimes(1);
+  });
+
+  it("chooses HlsJsTech if HLS and not Safari", async () => {
+    jest.spyOn(contentType, "getManifestType").mockImplementation(async () => ManifestType.HLS);
+    jest.spyOn(contentType, "canPlayManifestType").mockImplementation(() => false);
+    jest.spyOn(HlsJsTech.prototype, "load").mockImplementation(async () => { });
+
+    const videoElement = window.document.createElement("video");
+    const player = new WebPlayer({ video: videoElement });
+    await player.load("mock-stream.m3u8");
+    expect(HlsJsTech).toHaveBeenCalledTimes(1);
+  });
+
+  it("can be played inline on mobile browsers", async () => {
+    // Setup necessary mockups
+    const setAttributeStub = jest
+      .spyOn(window.HTMLVideoElement.prototype, "setAttribute")
+      .mockImplementation(() => {});
+    // Mock tech decisions to fall on Safari
+    jest.spyOn(contentType, "getManifestType").mockImplementation(async () => ManifestType.HLS);
+    jest.spyOn(contentType, "canPlayManifestType").mockImplementation(() => true);
+    jest.spyOn(browser, "isSafari").mockImplementation(() => true );
+    jest.spyOn(BaseTech.prototype, "load").mockImplementation(async () => { });
+
+    const videoElement = window.document.createElement("video");
+    const player = new WebPlayer({ video: videoElement });
+    await player.load("mock-stream");
+    expect(BaseTech).toHaveBeenCalledTimes(1);
+    expect(setAttributeStub).toHaveBeenCalledWith("playsinline", "");
+    setAttributeStub.mockRestore();
+  });
+});
