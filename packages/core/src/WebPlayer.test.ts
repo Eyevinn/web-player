5c01cb477921dd4d7a787879f6ddd13f8568c777
birme <jonas.birme@eyevinn.se>
2022-03-07 14:43:04 +0100

    chore: added HlsJsTech decision test

--- packages/core/src/WebPlayer.test.ts
+++ packages/core/src/WebPlayer.test.ts
@@ -5,6 +5,8 @@ import * as contentType from "./util/contentType";
 import * as browser from "./util/browser";
 import BaseTech from "./tech/BaseTech";
 jest.mock("./tech/BaseTech");
+import HlsJsTech from "./tech/HlsJsTech";
+jest.mock("./tech/HlsJsTech");
 
 import WebPlayer from "./WebPlayer";
 import { ManifestType } from "./util/constants";
@@ -27,6 +29,17 @@ describe("WebPlayer core", () => {
     const player = new WebPlayer({ video: videoElement });
     await player.load("mock-stream");
     expect(BaseTech).toHaveBeenCalledTimes(1);
+  });
+
+  it("chooses HlsJsTech if HLS and not Safari", async () => {
+    jest.spyOn(contentType, "getManifestType").mockImplementation(async () => ManifestType.HLS);
+    jest.spyOn(contentType, "canPlayManifestType").mockImplementation(() => false);
+    jest.spyOn(HlsJsTech.prototype, "load").mockImplementation(async () => { });
+
+    const videoElement = window.document.createElement("video");
+    const player = new WebPlayer({ video: videoElement });
+    await player.load("mock-stream.m3u8");
+    expect(HlsJsTech).toHaveBeenCalledTimes(1);
   });
 
   it("can be played inline on mobile browsers", async () => {
