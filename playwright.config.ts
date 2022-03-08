28dcffb63bb5cb1b36ca2c70f1e60e837aec2718
Jonas Birmé <jonas.birme@eyevinn.se>
2022-03-08 12:45:59 +0100

    chore: a foundation for integration / e2e tests (#64)
    * chore: example apps to be used for integration tests
    
    * chore: a foundation for e2e tests
    
    * chore: fix broken yaml

---
+++ playwright.config.ts
@@ -0,0 +1,31 @@
+import { PlaywrightTestConfig, devices } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  testDir: "tests/",
+  workers: 3,
+  use: {
+    trace: 'on-first-retry',
+  },
+  webServer: {
+    command: 'npm run examples',
+    port: 1234,
+    reuseExistingServer: !process.env.CI,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+};
+export default config;
