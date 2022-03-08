16db8247b15685f0e92fcd7f7ff4d52f29b218e7
birme <jonas.birme@eyevinn.se>
2022-03-08 22:10:21 +0100

    chore: check width and height

--- tests/web-component.spec.ts
+++ tests/web-component.spec.ts
@@ -1,16 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('video element with correct attributes are created', async ({ page }) => {
-  await page.goto('http://localhost:1234/web-component/index.html');
+  await page.goto('/web-component/index.html');
   await expect(page.locator('eyevinn-video')).toHaveAttribute('muted', '');
   await expect(page.locator('eyevinn-video')).toHaveAttribute('autoplay', '');
   await expect(page.locator('video')).toHaveAttribute('autoplay', '');
   await expect(page.locator('video')).toHaveAttribute('playsinline', '');
+  await expect(page.locator('video')).toHaveCSS('width', '1264px');
+  await expect(page.locator('video')).toHaveCSS('height', '712px');
 });
 
 test('web component sends tracking events when not in incognito mode', async ({ page }) => {
   const [request] = await Promise.all([
     page.waitForRequest(req => req.url().match('https://sink.epas') && req.method() === 'POST'),
-    page.goto('http://localhost:1234/web-component/index.html'),
+    page.goto('/web-component/index.html'),
   ]);
 });
