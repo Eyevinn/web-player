f27d43d8221243205634ce964f8a779298eb592d
bjowe <bjorn.westman@eyevinn.se>
2022-07-08 13:20:38 +0200

    Added Chaos Proxy Test for Dash

--- tests/chaos-proxy-hls.spec.ts
+++ tests/chaos-proxy-hls.spec.ts
@@ -1,9 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-  test('player sends error events when loading corrupt streams', async ({page}) => {
+  test('player sends error events when loading corrupt hls streams', async ({page }) => {
     const [request] = await Promise.all([
-      page.goto('/chaos-proxy/index.html'),
+      page.goto('/chaos-proxy/index_hls.html'),
       page.waitForRequest(req => req.url().match('https://sink.epas') && req.method() === 'POST' && req.postDataJSON().event === "warning" && req.postDataJSON().payload.code === "400"),
       page.waitForRequest(req => req.url().match('https://sink.epas') && req.method() === 'POST' && req.postDataJSON().event === "warning" && req.postDataJSON().payload.code === "404"),
+      
     ]);
-  });
+   }); 
+   
+  
+  
+
+
+
